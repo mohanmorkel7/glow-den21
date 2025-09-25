6574d1d0400a6371d7b113d3436a8d5d07384a1b
mohan.m@mindeed.in <mohan.m@mindeed.in>
2025-09-25 18:39:03 +0530

    new cde

--- docs/schema.sql
+++ docs/schema.sql
@@ -613,3 +613,28 @@ CREATE TABLE IF NOT EXISTS public.permissions (
     is_active boolean DEFAULT true NOT NULL,
     created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
+
+
+INSERT INTO users (
+  id, name, email, phone, hashed_password, role, status,
+  department, job_title, avatar_url, theme, language,
+  notifications_enabled, join_date, last_login, created_at, updated_at
+) VALUES (
+  '6c3cfd14-4862-48bf-9a98-16f18286428d',
+  'Super Admin',
+  'admin@websyntactic.com',
+  '9629558605',
+  '$2b$10$lg2cCZkN6y5i4wvaNCOXM.3KMelf8y/sug.Zccm72mAtaBLY/L7Tq',
+  'super_admin',
+  'active',
+  NULL,
+  NULL,
+  NULL,
+  'system',
+  'English',
+  true,
+  '2025-09-17',
+  '2025-09-24 06:02:46.6047',
+  '2025-09-17 06:13:44.566058',
+  '2025-09-24 06:02:46.6047'
+);
