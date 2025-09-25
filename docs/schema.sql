21bd5bee21f7b79a2b305ecc340e567b06c916a8
mohan.m@mindeed.in <mohan.m@mindeed.in>
2025-09-25 18:09:35 +0530

    new cde

--- docs/schema.sql
+++ docs/schema.sql
@@ -456,59 +456,59 @@ CREATE TABLE IF NOT EXISTS public.user_notifications (
 --
 -- Name: users; Type: TABLE; Schema: public; Owner: -
 --
-Drop table public.users;
+-- Drop table public.users;
 
-CREATE TABLE IF NOT EXISTS public.users (
-    id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
-    name character varying(255) NOT NULL,
-    email character varying(255) NOT NULL,
-    phone character varying(50),
-    hashed_password character varying(255) NOT NULL,
-    role public.user_role DEFAULT 'user'::public.user_role NOT NULL,
-    status public.user_status DEFAULT 'active'::public.user_status NOT NULL,
-    department character varying(100),
-    job_title character varying(100),
-    avatar_url text,
-    theme character varying(20) DEFAULT 'system'::character varying,
-    language character varying(10) DEFAULT 'English'::character varying,
-    notifications_enabled boolean DEFAULT true,
-    join_date date DEFAULT CURRENT_DATE NOT NULL,
-    last_login timestamp without time zone,
-    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
-);
-
-
---
--- Name: TABLE users; Type: COMMENT; Schema: public; Owner: -
---
-
-COMMENT ON TABLE public.users IS 'Core user accounts with authentication and profile information';
+-- CREATE TABLE IF NOT EXISTS public.users (
+--     id uuid DEFAULT public.uuid_generate_v4() NOT NULL,
+--     name character varying(255) NOT NULL,
+--     email character varying(255) NOT NULL,
+--     phone character varying(50),
+--     hashed_password character varying(255) NOT NULL,
+--     role public.user_role DEFAULT 'user'::public.user_role NOT NULL,
+--     status public.user_status DEFAULT 'active'::public.user_status NOT NULL,
+--     department character varying(100),
+--     job_title character varying(100),
+--     avatar_url text,
+--     theme character varying(20) DEFAULT 'system'::character varying,
+--     language character varying(10) DEFAULT 'English'::character varying,
+--     notifications_enabled boolean DEFAULT true,
+--     join_date date DEFAULT CURRENT_DATE NOT NULL,
+--     last_login timestamp without time zone,
+--     created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+--     updated_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
+-- );
 
 
-INSERT INTO users (
-  id, name, email, phone, hashed_password, role, status,
-  department, job_title, avatar_url, theme, language,
-  notifications_enabled, join_date, last_login, created_at, updated_at
-) VALUES (
-  '6c3cfd14-4862-48bf-9a98-16f18286428d',
-  'Super Admin',
-  'admin@websyntactic.com',
-  '9629558605',
-  '$2b$10$lg2cCZkN6y5i4wvaNCOXM.3KMelf8y/sug.Zccm72mAtaBLY/L7Tq',
-  'super_admin',
-  'active',
-  NULL,
-  NULL,
-  NULL,
-  'system',
-  'English',
-  true,
-  '2025-09-17',
-  '2025-09-24 06:02:46.6047',
-  '2025-09-17 06:13:44.566058',
-  '2025-09-24 06:02:46.6047'
-);
+-- --
+-- -- Name: TABLE users; Type: COMMENT; Schema: public; Owner: -
+-- --
+
+-- COMMENT ON TABLE public.users IS 'Core user accounts with authentication and profile information';
+
+
+-- INSERT INTO users (
+--   id, name, email, phone, hashed_password, role, status,
+--   department, job_title, avatar_url, theme, language,
+--   notifications_enabled, join_date, last_login, created_at, updated_at
+-- ) VALUES (
+--   '6c3cfd14-4862-48bf-9a98-16f18286428d',
+--   'Super Admin',
+--   'admin@websyntactic.com',
+--   '9629558605',
+--   '$2b$10$lg2cCZkN6y5i4wvaNCOXM.3KMelf8y/sug.Zccm72mAtaBLY/L7Tq',
+--   'super_admin',
+--   'active',
+--   NULL,
+--   NULL,
+--   NULL,
+--   'system',
+--   'English',
+--   true,
+--   '2025-09-17',
+--   '2025-09-24 06:02:46.6047',
+--   '2025-09-17 06:13:44.566058',
+--   '2025-09-24 06:02:46.6047'
+-- );
 
 --
 -- Name: dashboard_stats; Type: VIEW; Schema: public; Owner: -
