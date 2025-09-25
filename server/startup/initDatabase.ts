725251a2f3bb6014a72772ad9981dbf5c60dbfdf
mohan.m@mindeed.in <mohan.m@mindeed.in>
2025-09-25 17:51:22 +0530

    new cde

--- server/startup/initDatabase.ts
+++ server/startup/initDatabase.ts
@@ -7,7 +7,7 @@ export async function initDatabase() {
     const check = await pool.query(`
       SELECT EXISTS (
         SELECT FROM information_schema.tables 
-        WHERE table_name = 'ksadnksdf'
+        WHERE table_name = 'users'
       );
     `);
 
